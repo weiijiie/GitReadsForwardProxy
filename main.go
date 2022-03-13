a919cbb0c3f3837d33f969469c63c11bb80d4e85
Huang Weijie <h_weijie@yahoo.com>
2022-03-13 18:21:29 +0800

    feat: implement forward proxy

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	gitHubUsername := os.Getenv("GH_USERNAME")
+	if gitHubUsername == "" {
+		log.Fatal("GH_USERNAME not set")
+	}
+
+	gitHubToken := os.Getenv("GH_TOKEN")
+	if gitHubToken == "" {
+		log.Fatal("GH_TOKEN not set")
+	}
+
+	router := gin.Default()
+	gitHubClient := makeGitHubClient(gitHubUsername, gitHubToken)
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Any("/github/*path", func(c *gin.Context) {
+		path := c.Param("path")
+		queryParams := c.Request.URL.Query()
+
+		log.Printf("Request Method: %s", c.Request.Method)
+		log.Printf("Request Path: %s", path)
+		log.Printf("Request Query Params: %v", queryParams)
+
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Printf("Error reading request body: %+v", err)
+		}
+
+		resp, err := gitHubClient.R().
+			SetBody(body).
+			SetQueryParamsFromValues(queryParams).
+			Execute(c.Request.Method, path)
+
+		if err != nil {
+			log.Printf("Error sending request: %+v, err: %+v", c.Request, err)
+			return
+		}
+
+		contentType := resp.Header().Get("content-type")
+		log.Printf("Response Content Type: %s", contentType)
+		log.Printf("Response Status Code: %d", resp.StatusCode())
+
+		// copy response headers
+		for k, values := range resp.Header() {
+			for _, val := range values {
+				c.Writer.Header().Add(k, val)
+			}
+		}
+
+		// return the response as-is
+		c.Data(resp.StatusCode(), contentType, resp.Body())
+	})
+
+	log.Println("listening on", port)
+	log.Fatal(router.Run(":" + port))
+}
+
+func makeGitHubClient(authUsername, authToken string) *resty.Client {
+	client := resty.New().
+		SetBaseURL("https://api.github.com").
+		SetBasicAuth(authUsername, authToken).
+		SetTimeout(30 * time.Second)
+
+	return client
+}
